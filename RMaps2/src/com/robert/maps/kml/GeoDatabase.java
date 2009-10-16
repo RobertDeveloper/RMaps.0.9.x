0e7d0818cc6e3322d52691c872fb640632121232
RobertK506 <RobertK506@dc686b20-874f-11de-a332-fd40ed1fd0be>
2009-10-16 17:10:17 +0000

    SQLite leak problem

--- RMaps2/src/com/robert/maps/kml/GeoDatabase.java
+++ RMaps2/src/com/robert/maps/kml/GeoDatabase.java
@@ -10,11 +10,12 @@ import android.database.sqlite.SQLiteDatabase;
 import android.widget.Toast;
 
 import com.robert.maps.R;
+import com.robert.maps.utils.RSQLiteOpenHelper;
 import com.robert.maps.utils.Ut;
 
 public class GeoDatabase {
 	protected final Context mCtx;
-	private final SQLiteDatabase mDatabase;
+	private SQLiteDatabase mDatabase;
 	protected final SimpleDateFormat DATE_FORMAT_ISO8601 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
 
 	public GeoDatabase(Context ctx) {
@@ -82,6 +83,9 @@ public class GeoDatabase {
 		boolean ret = true;
 
 		if(mDatabase == null)
+			mDatabase = getDatabase();
+		
+		if(mDatabase == null)
 			ret = false;
 
 		if(ret == false)
@@ -89,23 +93,43 @@ public class GeoDatabase {
 
 		return ret;
 	}
+	
+	public void FreeDatabase(){
+		if(mDatabase != null){
+			mDatabase.close();
+			Ut.dd("Close database isOpen="+mDatabase.isOpen());
+			mDatabase = null;
+		}
+	}
 
 	protected SQLiteDatabase getDatabase() {
 		File folder = Ut.getRMapsFolder("data", false);
 		if(!folder.exists()) // no sdcard
 			return null;
+		
+		Ut.dd("open database");
+		SQLiteDatabase db = new GeoDatabaseHelper(mCtx, folder.getAbsolutePath() + "/geodata.db").getWritableDatabase();
 
-		SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase(folder.getAbsolutePath() + "/geodata.db", null);
+		return db;
+	}
+	
+	protected class GeoDatabaseHelper extends RSQLiteOpenHelper {
+		public GeoDatabaseHelper(final Context context, final String name) {
+			super(context, name, null, 1);
+		}
 
-		// for ver.1
-		if(db.needUpgrade(1)) {
-			Ut.dd("Upgrade data.db from ver.0 to ver.1");
+		@Override
+		public void onCreate(SQLiteDatabase db) {
 			db.execSQL("CREATE TABLE IF NOT EXISTS 'points' (pointid INTEGER NOT NULL PRIMARY KEY UNIQUE, name VARCHAR, descr VARCHAR, lat FLOAT DEFAULT '0', lon FLOAT DEFAULT '0', alt FLOAT DEFAULT '0', categoryid INTEGER, pointsourceid INTEGER);");
 			db.execSQL("CREATE TABLE IF NOT EXISTS 'pointsource' (pointsourceid INTEGER NOT NULL PRIMARY KEY UNIQUE, name VARCHAR);");
 			db.execSQL("CREATE TABLE IF NOT EXISTS 'category' (categoryid INTEGER NOT NULL PRIMARY KEY UNIQUE, name VARCHAR);");
-			db.setVersion(1);
 		}
 
-		return db;
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+//			Ut.dd("Upgrade data.db from ver.0 to ver.1");
+			// for ver.1
+		}
+		
 	}
 }
