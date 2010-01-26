b3743112ee81d6f4d639034d0bda7e1e8d1a056e
RobertK506 <RobertK506@dc686b20-874f-11de-a332-fd40ed1fd0be>
2010-01-26 06:08:47 +0000

    Import POI

--- RMaps2/src/com/robert/maps/kml/PoiListActivity.java
+++ RMaps2/src/com/robert/maps/kml/PoiListActivity.java
@@ -1,5 +1,18 @@
 package com.robert.maps.kml;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
+import org.xml.sax.SAXException;
+
 import android.app.ListActivity;
 import android.content.Intent;
 import android.database.Cursor;
@@ -16,10 +29,11 @@ import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
 import com.robert.maps.R;
+import com.robert.maps.utils.Ut;
 
 public class PoiListActivity extends ListActivity {
 	private PoiManager mPoiManager;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -50,8 +64,8 @@ public class PoiListActivity extends ListActivity {
         startManagingCursor(c);
 
         ListAdapter adapter = new SimpleCursorAdapter(this,
-                android.R.layout.simple_list_item_2, c, 
-                        new String[] { "name", "descr" }, 
+                android.R.layout.simple_list_item_2, c,
+                        new String[] { "name", "descr" },
                         new int[] { android.R.id.text1, android.R.id.text2 });
         setListAdapter(adapter);
 	}
@@ -69,7 +83,7 @@ public class PoiListActivity extends ListActivity {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		super.onOptionsItemSelected(item);
-		
+
 		switch(item.getItemId()){
 		case R.id.menu_addpoi:
 			startActivity((new Intent(this, PoiActivity.class)));
@@ -77,9 +91,56 @@ public class PoiListActivity extends ListActivity {
 		case R.id.menu_categorylist:
 			startActivity((new Intent(this, PoiCategoryListActivity.class)));
 			return true;
+		case R.id.menu_importpoi:
+			doImportPOI();
+			return true;
 		}
-		
+
 		return true;
+	}
+
+	private void doImportPOI() {
+		Ut.getRMapsFolder("import", false);
+		File file = new File("/sdcard/rmaps/import/1.kml");
+
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db = null;
+		try {
+			db = dbf.newDocumentBuilder();
+		} catch (ParserConfigurationException e1) {
+			e1.printStackTrace();
+		}
+		Document doc = null;
+		try {
+			doc = db.parse(file);
+
+			NodeList nl = doc.getDocumentElement().getElementsByTagName("Placemark");
+			NodeList plk = null;
+			Node node = null;
+			for(int i = 0; i < nl.getLength(); i++){
+				Ut.dd("placemark"+i);
+				plk = nl.item(i).getChildNodes();
+				PoiPoint poi = new PoiPoint();
+				for(int j = 0; j < plk.getLength(); j++){
+					node = plk.item(j); //node.getLocalName() node.getNodeType()
+					if(node.getNodeName()=="name"){
+						//Text t = ((Text)(plk.item(j))).getData();
+						poi.Title = node.getNodeValue();
+					}
+					else if(node.getNodeName()=="description")
+						poi.Descr = node.getNodeValue();
+					else if(node.getNodeName()=="Point"){
+
+					}
+				}
+				Ut.dd(poi.Title+" :: "+poi.Descr);
+			}
+		} catch (SAXException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
 	}
 
 	@Override
@@ -87,7 +148,7 @@ public class PoiListActivity extends ListActivity {
 			ContextMenuInfo menuInfo) {
 		int pointid = (int) ((AdapterView.AdapterContextMenuInfo)menuInfo).id;
 		PoiPoint poi = mPoiManager.getPoiPoint(pointid);
-		
+
 		menu.add(0, R.id.menu_gotopoi, 0, getText(R.string.menu_goto));
 		menu.add(0, R.id.menu_editpoi, 0, getText(R.string.menu_edit));
 		if(poi.Hidden)
@@ -95,7 +156,7 @@ public class PoiListActivity extends ListActivity {
 		else
 			menu.add(0, R.id.menu_hide, 0, getText(R.string.menu_hide));
 		menu.add(0, R.id.menu_deletepoi, 0, getText(R.string.menu_delete));
-		
+
 		super.onCreateContextMenu(menu, v, menuInfo);
 	}
 
@@ -103,7 +164,7 @@ public class PoiListActivity extends ListActivity {
 	public boolean onContextItemSelected(MenuItem item) {
 		int pointid = (int) ((AdapterView.AdapterContextMenuInfo)item.getMenuInfo()).id;
 		PoiPoint poi = mPoiManager.getPoiPoint(pointid);
-		
+
 		switch(item.getItemId()){
 		case R.id.menu_editpoi:
 			startActivity((new Intent(this, PoiActivity.class)).putExtra("pointid", pointid));
@@ -127,7 +188,7 @@ public class PoiListActivity extends ListActivity {
 			FillData();
 	        break;
 		}
-		
+
 		return super.onContextItemSelected(item);
 	}
 
