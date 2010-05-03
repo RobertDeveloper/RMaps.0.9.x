c508704cfb29f2a65625ab66b486a4697157f192
RobertK506 <RobertK506@dc686b20-874f-11de-a332-fd40ed1fd0be>
2010-05-03 19:31:22 +0000

    Bad track if moving map

--- RMaps2/src/com/robert/maps/overlays/TrackOverlay.java
+++ RMaps2/src/com/robert/maps/overlays/TrackOverlay.java
@@ -1,20 +1,27 @@
 package com.robert.maps.overlays;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.andnav.osm.util.GeoPoint;
 import org.andnav.osm.views.OpenStreetMapView;
 import org.andnav.osm.views.OpenStreetMapView.OpenStreetMapViewProjection;
 import org.andnav.osm.views.overlay.OpenStreetMapViewOverlay;
+import org.andnav.osm.views.util.OpenStreetMapTileFilesystemProvider;
 
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
+import android.os.Handler;
+import android.os.Message;
 
 import com.robert.maps.MainMapActivity;
 import com.robert.maps.R;
 import com.robert.maps.kml.PoiManager;
 import com.robert.maps.kml.Track;
 import com.robert.maps.kml.Track.TrackPoint;
+import com.robert.maps.utils.Ut;
 
 public class TrackOverlay extends OpenStreetMapViewOverlay {
 	private Paint mPaint;
@@ -23,12 +30,81 @@ public class TrackOverlay extends OpenStreetMapViewOverlay {
 	private Track mTrack;
 	private Point mBaseCoords;
 	private GeoPoint mBaseLocation;
+	private PoiManager mPoiManager;
+	private TrackThread mThread;
+	private boolean mThreadRunned = false;
+	private OpenStreetMapView mOsmv;
+	private Handler mMainMapActivityCallbackHandler;
+
+	protected ExecutorService mThreadExecutor = Executors.newSingleThreadExecutor();
+
+	private class TrackThread extends Thread {
+
+		@Override
+		public void run() {
+			Ut.dd("run TrackThread");
+
+			mPath = null;
+			final Path path = new Path();
+
+			if(mTrack == null){
+				mTrack = mPoiManager.getTrack(1);
+				Ut.dd("Track loaded");
+			}
+
+			final OpenStreetMapViewProjection pj = mOsmv.getProjection();
+//			pj.toPixelsTrackPoints(mTrack.trackpoints, path);
+			mPath =path;
+
+			final Point screenCoords = new Point();
+			final Point screenCoords2 = new Point();
+			final GeoPoint loc = new GeoPoint(0, 0);
+
+			pj.toPixels(mBaseLocation, mBaseCoords);
+			int i = 0;
+			boolean isFirstPoint = true;
+			for (TrackPoint trackpoint : mTrack.trackpoints) {
+				loc.setCoordsE6((int) (trackpoint.lat * 1E6), (int) (trackpoint.lon * 1E6));
+				pj.toPixels(loc, screenCoords);
+
+				if(!isFirstPoint){
+					if (Math.abs(screenCoords2.x - screenCoords.x) > 5
+							|| Math.abs(screenCoords2.y - screenCoords.y) > 5) {
+						path.lineTo(screenCoords.x, screenCoords.y);
+						screenCoords2.x = screenCoords.x;
+						screenCoords2.y = screenCoords.y;
+						i++;
+						if(i<10) Ut.dd("test coord "+screenCoords.x+" "+screenCoords.y);
+					}
+				} else {
+					path.setLastPoint(screenCoords.x, screenCoords.y);
+					screenCoords2.x = screenCoords.x;
+					screenCoords2.y = screenCoords.y;
+					isFirstPoint = false;
+					i++;
+				}
+			}
+
+			mPath = path;
+
+			Ut.dd("Track maped");
+
+			Message.obtain(mMainMapActivityCallbackHandler, OpenStreetMapTileFilesystemProvider.MAPTILEFSLOADER_SUCCESS_ID).sendToTarget();
+
+			mThreadRunned = false;
+			//super.run();
+		}
+	}
 
 	public TrackOverlay(MainMapActivity mainMapActivity, PoiManager poiManager) {
-		mTrack = poiManager.getTrack(1);
+		mTrack = null;
+		mPoiManager = poiManager;
 		mBaseCoords = new Point();
 		mBaseLocation = new GeoPoint(0, 0);
 		mLastZoom = -1;
+		mThread = new TrackThread();
+		mThread.setName("Track thread");
+
 
 		mPaint = new Paint();
 		mPaint.setAntiAlias(true);
@@ -39,40 +115,25 @@ public class TrackOverlay extends OpenStreetMapViewOverlay {
 
 	@Override
 	protected void onDraw(Canvas c, OpenStreetMapView osmv) {
-		if(mTrack == null)
+		if (!mThreadRunned && (mTrack == null || mLastZoom != osmv.getZoomLevel())) {
+			mPath = null;
+			mLastZoom = osmv.getZoomLevel();
+			mMainMapActivityCallbackHandler = osmv.getHandler();
+			mOsmv = osmv;
+			//mThread.run();
+			Ut.dd("mThreadExecutor.execute "+mThread.isAlive());
+			mThreadRunned = true;
+			mThreadExecutor.execute(mThread);
+			return;
+		}
+
+		if(mPath == null)
 			return;
 
+		Ut.dd("Draw track");
 		final OpenStreetMapViewProjection pj = osmv.getProjection();
 		final Point screenCoords = new Point();
-		final Point screenCoords2 = new Point();
-		final GeoPoint loc = new GeoPoint(0, 0);
 
-		if (mLastZoom != osmv.getZoomLevel()) {
-			mLastZoom = osmv.getZoomLevel();
-			pj.toPixels(mBaseLocation, mBaseCoords);
-
-			mPath = new Path();
-
-			boolean isFirstPoint = true;
-			for (TrackPoint trackpoint : mTrack.trackpoints) {
-				loc.setCoordsE6((int) (trackpoint.lat * 1E6), (int) (trackpoint.lon * 1E6));
-				pj.toPixels(loc, screenCoords);
-
-				if(!isFirstPoint){
-					if (Math.abs(screenCoords2.x - screenCoords.x) > 5
-							|| Math.abs(screenCoords2.y - screenCoords.y) > 5) {
-						mPath.lineTo(screenCoords.x, screenCoords.y);
-						screenCoords2.x = screenCoords.x;
-						screenCoords2.y = screenCoords.y;
-					}
-				} else {
-					mPath.setLastPoint(screenCoords.x, screenCoords.y);
-					screenCoords2.x = screenCoords.x;
-					screenCoords2.y = screenCoords.y;
-					isFirstPoint = false;
-				}
-			}
-		}
 		pj.toPixels(mBaseLocation, screenCoords);
 
 		//final long startMs = System.currentTimeMillis();
