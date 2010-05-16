510ab2c0dbebf19be540cc0993234a870dd8d590
RobertK506 <RobertK506@dc686b20-874f-11de-a332-fd40ed1fd0be>
2010-05-16 20:04:51 +0000

    v.0.8.2

---
+++ tags/v.0.8.2/RMaps2/src/com/robert/maps/MainPreferences.java
@@ -0,0 +1,193 @@
+/**
+ *
+ */
+package com.robert.maps;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceGroup;
+import android.preference.PreferenceScreen;
+
+import com.robert.maps.utils.Ut;
+
+public class MainPreferences extends PreferenceActivity implements OnSharedPreferenceChangeListener {
+	private static final String PREF_USERMAPS_ = "pref_usermaps_";
+	private static final String PREF_PREDEFMAPS_ = "pref_predefmaps_";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Load the preferences from an XML resource
+		addPreferencesFromResource(R.xml.mainpreferences);
+
+		PreferenceGroup prefMapsgroup = (PreferenceGroup) findPreference("pref_predefmaps_mapsgroup");
+
+		InputStream in = getResources().openRawResource(R.raw.predefmaps);
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db = null;
+		try {
+			db = dbf.newDocumentBuilder();
+		} catch (ParserConfigurationException e1) {
+			e1.printStackTrace();
+		}
+		Document doc = null;
+		try {
+			doc = db.parse(in);
+
+			NodeList nl = doc.getDocumentElement().getElementsByTagName("map");
+			for (int i = 0; i < nl.getLength(); i++) {
+				NamedNodeMap nnm = nl.item(i).getAttributes();
+
+				Node nodeLayer = nnm.getNamedItem("layer");
+				boolean ItIsLayer = false;
+				if (nodeLayer != null)
+					ItIsLayer = nodeLayer.getNodeValue().equalsIgnoreCase("true");
+
+				if (!ItIsLayer) {
+					CheckBoxPreference pref = new CheckBoxPreference(this);
+					pref.setKey(PREF_PREDEFMAPS_ + nnm.getNamedItem("id").getNodeValue());
+					pref.setTitle(nnm.getNamedItem("name").getNodeValue());
+					pref.setSummary(nnm.getNamedItem("descr").getNodeValue());
+					pref.setDefaultValue(true);
+					prefMapsgroup.addPreference(pref);
+				}
+			}
+		} catch (SAXException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+		LoadUserMaps();
+	}
+
+	private void LoadUserMaps() {
+		// Cash file preferences
+		PreferenceGroup prefUserMapsgroup = (PreferenceGroup) findPreference("pref_usermaps_mapsgroup");
+		prefUserMapsgroup.removeAll();
+
+		File folder = Ut.getRMapsFolder("maps", true);
+		File[] files = folder.listFiles();
+		if (files != null)
+			for (int i = 0; i < files.length; i++) {
+				if (files[i].getName().toLowerCase().endsWith(
+						getString(R.string.mnm))
+						|| files[i].getName().toLowerCase().endsWith(
+								getString(R.string.tar))
+						|| files[i].getName().toLowerCase().endsWith(
+								getString(R.string.sqlitedb))) {
+					String name = Ut.FileName2ID(files[i].getName());
+
+					PreferenceScreen prefscr = getPreferenceManager().createPreferenceScreen(this);
+					prefscr.setKey(PREF_USERMAPS_ + name);
+					{
+						CheckBoxPreference pref = new CheckBoxPreference(this);
+						pref.setKey(PREF_USERMAPS_ + name + "_enabled");
+						pref.setTitle(getString(R.string.pref_usermap_enabled));
+						pref.setSummary(getString(R.string.pref_usermap_enabled_summary));
+						pref.setDefaultValue(false);
+						prefscr.addPreference(pref);
+					}
+					{
+						EditTextPreference pref = new EditTextPreference(this);
+						pref.setKey(PREF_USERMAPS_ + name + "_name");
+						pref.setTitle(getString(R.string.pref_usermap_name));
+						pref.setSummary(files[i].getName());
+						pref.setDefaultValue(files[i].getName());
+						prefscr.addPreference(pref);
+					}
+					{
+						EditTextPreference pref = new EditTextPreference(this);
+						pref.setKey(PREF_USERMAPS_ + name + "_baseurl");
+						pref.setTitle(getString(R.string.pref_usermap_baseurl));
+						pref.setSummary(files[i].getAbsolutePath());
+						pref.setDefaultValue(files[i].getAbsolutePath());
+						pref.setEnabled(false);
+						prefscr.addPreference(pref);
+					}
+					{
+						ListPreference pref = new ListPreference(this);
+						pref.setKey(PREF_USERMAPS_ + name + "_projection");
+						pref.setTitle(getString(R.string.pref_usermap_projection));
+						pref.setEntries(R.array.projection_title);
+						pref.setEntryValues(R.array.projection_value);
+						pref.setDefaultValue("1");
+						prefscr.addPreference(pref);
+						pref.setSummary(pref.getEntry());
+					}
+					{
+						CheckBoxPreference pref = new CheckBoxPreference(this);
+						pref.setKey(PREF_USERMAPS_ + name + "_traffic");
+						pref.setTitle(getString(R.string.pref_usermap_traffic));
+						pref.setSummary(getString(R.string.pref_usermap_traffic_summary));
+						pref.setDefaultValue(false);
+						prefscr.addPreference(pref);
+					}
+
+					prefscr.setTitle(prefscr.getSharedPreferences().getString(PREF_USERMAPS_ + name + "_name",
+							files[i].getName()));
+					if (prefscr.getSharedPreferences().getBoolean(PREF_USERMAPS_ + name + "_enabled", false))
+						prefscr.setSummary("Enabled  " + files[i].getAbsolutePath());
+					else
+						prefscr.setSummary("Disabled  " + files[i].getAbsolutePath());
+					prefUserMapsgroup.addPreference(prefscr);
+				}
+			}
+	}
+
+	@Override
+	    protected void onResume() {
+	        super.onResume();
+
+	        // Set up a listener whenever a key changes
+	        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+	    }
+
+	    @Override
+	    protected void onPause() {
+	        super.onPause();
+
+	        // Unregister the listener whenever a key changes
+	        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+	    }
+
+	public void onSharedPreferenceChanged(SharedPreferences aPref, String aKey) {
+		if (aKey.length() > 14)
+			if (aKey.substring(0, 14).equalsIgnoreCase(PREF_USERMAPS_)) {
+				if (aKey.endsWith("name")) {
+					findPreference(aKey).setSummary(aPref.getString(aKey, ""));
+					findPreference(aKey.replace("_name", "")).setTitle(aPref.getString(aKey, ""));
+				} else if (aKey.endsWith("enabled")) {
+					if (aPref.getBoolean(aKey, false))
+						findPreference(aKey.replace("_enabled", "")).setSummary(
+								"Enabled  " + aPref.getString(aKey.replace("_enabled", "_baseurl"), ""));
+					else
+						findPreference(aKey.replace("_enabled", "")).setSummary(
+								"Disabled  " + aPref.getString(aKey.replace("_enabled", "_baseurl"), ""));
+				} else if (aKey.endsWith("projection")) {
+					ListPreference pref = (ListPreference) findPreference(aKey);
+					findPreference(aKey).setSummary(pref.getEntry());
+				}
+			}
+	}
+}
---
+++ tags/v.0.8.2/RMaps2/src/com/robert/maps/kml/PoiManager.java
@@ -0,0 +1,222 @@
+package com.robert.maps.kml;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.andnav.osm.util.GeoPoint;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+
+import com.robert.maps.R;
+import com.robert.maps.kml.Track.TrackPoint;
+
+
+public class PoiManager {
+	protected final Context mCtx;
+	private GeoDatabase mGeoDatabase;
+
+	public PoiManager(Context ctx) {
+		super();
+		mCtx = ctx;
+		mGeoDatabase = new GeoDatabase(ctx);
+	}
+
+	public GeoDatabase getGeoDatabase(){
+		return mGeoDatabase;
+	}
+
+	public void FreeDatabases(){
+		mGeoDatabase.FreeDatabases();
+	}
+
+	public void addPoi(final String title, final String descr, GeoPoint point){
+		mGeoDatabase.addPoi(title, descr, point.getLatitude(), point.getLongitude(), 0, 0, 0, 0, R.drawable.poi);
+	}
+
+	public void updatePoi(final PoiPoint point){
+		if(point.getId() < 0)
+			mGeoDatabase.addPoi(point.Title, point.Descr, point.GeoPoint.getLatitude(), point.GeoPoint.getLongitude(), point.Alt, point.CategoryId, point.PointSourceId, point.Hidden == true ? 1 : 0, point.IconId);
+		else
+			mGeoDatabase.updatePoi(point.getId(), point.Title, point.Descr, point.GeoPoint.getLatitude(), point.GeoPoint.getLongitude(), point.Alt, point.CategoryId, point.PointSourceId, point.Hidden == true ? 1 : 0, point.IconId);
+	}
+
+	private List<PoiPoint> doCreatePoiListFromCursor(Cursor c){
+		final ArrayList<PoiPoint> items = new ArrayList<PoiPoint>();
+		if (c != null) {
+			if (c.moveToFirst()) {
+				do {
+					items.add(new PoiPoint(c.getInt(4), c.getString(2), c.getString(3), new GeoPoint(
+							(int) (1E6 * c.getDouble(0)), (int) (1E6 * c.getDouble(1))), c.getInt(7), c.getInt(8)));
+				} while (c.moveToNext());
+			}
+			c.close();
+		}
+
+		return items;
+	}
+
+	public List<PoiPoint> getPoiList() {
+		return doCreatePoiListFromCursor(mGeoDatabase.getPoiListCursor());
+	}
+
+	public List<PoiPoint> getPoiListNotHidden(int zoom, GeoPoint center, double deltaX, double deltaY){
+		return doCreatePoiListFromCursor(mGeoDatabase.getPoiListNotHiddenCursor(zoom, center.getLongitude() - deltaX, center.getLongitude() + deltaX
+				, center.getLatitude() + deltaY, center.getLatitude() - deltaY));
+	}
+
+	public void addPoiStartActivity(Context ctx, GeoPoint touchDownPoint) {
+		ctx.startActivity((new Intent(ctx, PoiActivity.class)).putExtra("lat",
+				touchDownPoint.getLatitude()).putExtra("lon",
+				touchDownPoint.getLongitude()));
+	}
+
+
+	public PoiPoint getPoiPoint(int id) {
+		PoiPoint point = null;
+		final Cursor c = mGeoDatabase.getPoi(id);
+		if (c != null) {
+			if (c.moveToFirst())
+				point = new PoiPoint(c.getInt(4), c.getString(2), c
+						.getString(3), new GeoPoint(
+						(int) (1E6 * c.getDouble(0)), (int) (1E6 * c
+								.getDouble(1))), c.getInt(9), c.getInt(7), c
+						.getInt(5), c.getInt(8), c.getInt(6));
+			c.close();
+		}
+
+		return point;
+	}
+
+	public void deletePoi(final int id){
+		mGeoDatabase.deletePoi(id);
+	}
+
+	public void deletePoiCategory(final int id){
+		mGeoDatabase.deletePoiCategory(id);
+	}
+
+	public PoiCategory getPoiCategory(int id) {
+		PoiCategory category = null;
+		final Cursor c = mGeoDatabase.getPoiCategory(id);
+		if (c != null) {
+			if (c.moveToFirst())
+				category = new PoiCategory(id, c.getString(0), c.getInt(2) == 1 ? true : false, c.getInt(3), c.getInt(4));
+			c.close();
+		}
+
+		return category;
+	}
+
+	public void updatePoiCategory(PoiCategory poiCategory) {
+		if(poiCategory.getId() < 0)
+			mGeoDatabase.addPoiCategory(poiCategory.Title, poiCategory.Hidden == true ? 1 : 0, poiCategory.IconId);
+		else
+			mGeoDatabase.updatePoiCategory(poiCategory.getId(), poiCategory.Title, poiCategory.Hidden == true ? 1 : 0, poiCategory.IconId, poiCategory.MinZoom);
+	}
+
+	public void DeleteAllPoi() {
+		mGeoDatabase.DeleteAllPoi();
+	}
+
+	public void beginTransaction(){
+		mGeoDatabase.beginTransaction();
+	}
+
+	public void rollbackTransaction(){
+		mGeoDatabase.rollbackTransaction();
+	}
+
+	public void commitTransaction(){
+		mGeoDatabase.commitTransaction();
+	}
+
+	public void updateTrack(Track track) {
+		if(track.getId() < 0){
+			long newId = mGeoDatabase.addTrack(track.Name, track.Descr, track.Show ? 1: 0);
+
+			for(TrackPoint trackpoint: track.getPoints()){
+				//Ut.dd("lat="+trackpoint.lat);
+				mGeoDatabase.addTrackPoint(newId, trackpoint.lat, trackpoint.lon, trackpoint.alt, trackpoint.speed, trackpoint.date);
+			}
+		} else
+			mGeoDatabase.updateTrack(track.getId(), track.Name, track.Descr, track.Show ? 1: 0);
+	}
+	
+	public boolean haveTrackChecked(){
+		boolean ret = false;
+		Cursor c = mGeoDatabase.getTrackChecked();
+		if (c != null) {
+			if (c.moveToFirst())
+				ret = true;
+			c.close();
+		}
+		
+		return ret;
+	}
+
+	public Track getTrackChecked(){
+		Track track = null;
+		Cursor c = mGeoDatabase.getTrackChecked();
+		if (c != null) {
+			if (c.moveToFirst())
+				track = new Track(c.getInt(3), c.getString(0), c.getString(1), c.getInt(2) == 1 ? true : false);
+			else {
+				c.close();
+				return null;
+			}
+			c.close();
+
+			c = mGeoDatabase.getTrackPoints(track.getId());
+			if (c != null) {
+				if (c.moveToFirst()) {
+					do {
+						track.AddTrackPoint(); //track.trackpoints.size()
+						track.LastTrackPoint.lat = c.getDouble(0);
+						track.LastTrackPoint.lon = c.getDouble(1);
+					} while (c.moveToNext());
+				}
+				c.close();
+			}
+
+		}
+		return track;
+	}
+
+	public Track getTrack(int id){
+		Track track = null;
+		Cursor c = mGeoDatabase.getTrack(id);
+		if (c != null) {
+			if (c.moveToFirst())
+				track = new Track(id, c.getString(0), c.getString(1), c.getInt(2) == 1 ? true : false);
+			c.close();
+			c = null;
+
+			c = mGeoDatabase.getTrackPoints(id);
+			if (c != null) {
+				if (c.moveToFirst()) {
+					do {
+						track.AddTrackPoint();
+						track.LastTrackPoint.lat = c.getDouble(0);
+						track.LastTrackPoint.lon = c.getDouble(1);
+					} while (c.moveToNext());
+				}
+				c.close();
+			}
+
+		}
+
+		return track;
+	}
+
+	public void setTrackChecked(int id) {
+		mGeoDatabase.setTrackChecked(id);
+	}
+
+	public void deleteTrack(int id) {
+		mGeoDatabase.deleteTrack(id);
+
+	}
+
+}
