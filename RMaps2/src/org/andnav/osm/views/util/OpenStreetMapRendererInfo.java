9155a57b32413d81a5f04f003cd9121bdd9bcbda
RobertK506 <RobertK506@dc686b20-874f-11de-a332-fd40ed1fd0be>
2011-01-30 22:54:23 +0000

    parsing predef maps

--- RMaps2/src/org/andnav/osm/views/util/OpenStreetMapRendererInfo.java
+++ RMaps2/src/org/andnav/osm/views/util/OpenStreetMapRendererInfo.java
@@ -4,26 +4,22 @@ package org.andnav.osm.views.util;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
 import java.util.Date;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 import org.andnav.osm.views.util.constants.OpenStreetMapViewConstants;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.xml.sax.SAXException;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 
 import com.robert.maps.R;
+import com.robert.maps.kml.XMLparser.PredefMapsParser;
 
 /**
  *
@@ -36,9 +32,9 @@ public class OpenStreetMapRendererInfo {
 	// ===========================================================
 
 	private final Resources mResources;
-	private int MAPTILE_SIZEPX;
-	private final int OpenSpaceUpperBoundArray[] = { 2, 5, 10, 25 , 50, 100, 200, 500, 1000, 2000, 4000};
-	private final int OpenSpaceLayersArray[] = {2500, 1000, 500, 200, 100, 50, 25, 10, 5, 2, 1};
+	public int MAPTILE_SIZEPX;
+	private static final int OpenSpaceUpperBoundArray[] = { 2, 5, 10, 25 , 50, 100, 200, 500, 1000, 2000, 4000};
+	private static final int OpenSpaceLayersArray[] = {2500, 1000, 500, 200, 100, 50, 25, 10, 5, 2, 1};
 
 	public String ID, BASEURL, NAME, IMAGE_FILENAMEENDING, GOOGLE_LANG_CODE;
 	public int ZOOM_MINLEVEL, ZOOM_MAXLEVEL,
@@ -106,36 +102,21 @@ public class OpenStreetMapRendererInfo {
 			else
 				this.YANDEX_TRAFFIC_ON = 0;
 		} else {
-			InputStream in = mResources.openRawResource(R.raw.predefmaps);
-			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			DocumentBuilder db = null;
+			SAXParserFactory fac = SAXParserFactory.newInstance();
+			SAXParser parser = null;
 			try {
-				db = dbf.newDocumentBuilder();
-			} catch (ParserConfigurationException e1) {
-				e1.printStackTrace();
+				parser = fac.newSAXParser();
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-			Document doc = null;
-			try {
-				doc = db.parse(in);
-				Element el = doc.getElementById(aId);
-				if(el == null)
-					el = doc.getElementById("mapnik");
 
-				this.ID = el.getAttribute("id");
-				this.NAME = el.getAttribute("name");
-				this.BASEURL = el.getAttribute("baseurl");
-				this.ZOOM_MINLEVEL = Integer.parseInt(el.getAttribute("ZOOM_MINLEVEL"));
-				this.ZOOM_MAXLEVEL = Integer.parseInt(el.getAttribute("ZOOM_MAXLEVEL"));
-				this.IMAGE_FILENAMEENDING = el.getAttribute("IMAGE_FILENAMEENDING");
-				this.MAPTILE_SIZEPX = Integer.parseInt(el.getAttribute("MAPTILE_SIZEPX"));
-				this.URL_BUILDER_TYPE = Integer.parseInt(el.getAttribute("URL_BUILDER_TYPE"));
-				this.TILE_SOURCE_TYPE = Integer.parseInt(el.getAttribute("TILE_SOURCE_TYPE"));
-				this.PROJECTION = Integer.parseInt(el.getAttribute("PROJECTION"));
-				this.YANDEX_TRAFFIC_ON = Integer.parseInt(el.getAttribute("YANDEX_TRAFFIC_ON"));
-			} catch (SAXException e1) {
-				e1.printStackTrace();
-			} catch (IOException e1) {
-				e1.printStackTrace();
+			if(parser != null){
+				try {
+					InputStream in = mResources.openRawResource(R.raw.predefmaps);
+					parser.parse(in, new PredefMapsParser(this, aId));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
