e4badb383a1088c6e0b6e2aa48821cd0f7b3b6cf
robertk506 <RobertK506@gmail.com>
2012-07-18 20:45:28 +0000

    FSCacheProvider implements ICacheProvider

---
+++ RMaps2/src/com/robert/maps/tileprovider/FSCacheProvider.java
@@ -0,0 +1,22 @@
+package com.robert.maps.tileprovider;
+
+import com.robert.maps.utils.ICacheProvider;
+
+public class FSCacheProvider implements ICacheProvider {
+
+	public byte[] getTile(String aURLstring, int aX, int aY, int aZ) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void putTile(String aURLstring, int aX, int aY, int aZ, byte[] aData) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void Free() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
