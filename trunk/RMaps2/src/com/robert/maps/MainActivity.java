e32b795fa2ae933bf31f57a48cb12c836ed680ff
RobertK506@gmail.com <RobertK506@gmail.com@dc686b20-874f-11de-a332-fd40ed1fd0be>
2012-07-23 15:34:11 +0000

    Debug

--- trunk/RMaps2/src/com/robert/maps/MainActivity.java
+++ trunk/RMaps2/src/com/robert/maps/MainActivity.java
@@ -188,7 +188,7 @@ public class MainActivity extends Activity {
 			
 			mTracker.setCustomVar(1, "Build", Ut.getAppVersion(this), 1);
 			mTracker.setCustomVar(2, "Ver", "Free", 1);
-			mTracker.setCustomVar(3, "DisplaySize", ""+metrics.widthPixels+"x"+metrics.heightPixels, 1);
+			mTracker.setCustomVar(3, "DisplaySize", ""+Math.min(metrics.widthPixels, metrics.heightPixels)+"x"+Math.max(metrics.widthPixels, metrics.heightPixels), 1);
 			mTracker.setCustomVar(4, "DisplayDensity", ""+(int)(160*metrics.density), 1);
 			mTracker.setCustomVar(5, "APILevel", Build.VERSION.SDK, 1);
 			mTracker.trackPageView("/InstallApp");
