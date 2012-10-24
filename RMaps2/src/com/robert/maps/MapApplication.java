8c77e33ab00b3bd938e470c25da1949046267224
robertk506 <RobertK506@gmail.com>
2012-10-24 19:52:15 +0000

    ČESKÝ localize

--- RMaps2/src/com/robert/maps/MapApplication.java
+++ RMaps2/src/com/robert/maps/MapApplication.java
@@ -40,12 +40,22 @@ public class MapApplication extends Application {
 	@Override
 	public void onConfigurationChanged(Configuration newConfig) {
 		super.onConfigurationChanged(newConfig);
-		if (locale != null)
-        {
-            newConfig.locale = locale;
-            Locale.setDefault(locale);
-            getBaseContext().getResources().updateConfiguration(newConfig, getBaseContext().getResources().getDisplayMetrics());
-        }
+		SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+        Configuration config = getBaseContext().getResources().getConfiguration();
+        defLocale = config.locale;
+        locale = defLocale;
+        
+        String lang = settings.getString("pref_locale", "");
+		if(lang.equalsIgnoreCase("zh_CN")) {
+			locale = Locale.SIMPLIFIED_CHINESE;
+		} else if(lang.equalsIgnoreCase("zh_TW")) {
+			locale = Locale.TRADITIONAL_CHINESE;
+		} else if(!lang.equalsIgnoreCase("") && !lang.equalsIgnoreCase(" ")) {
+            locale = new Locale(lang);
+		} 
+        Locale.setDefault(locale);
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
 	}
 	
 }
