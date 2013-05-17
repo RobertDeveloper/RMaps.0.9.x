7d260de70fff3bbd3728220f0ad7008434f62c57
robertk506 <RobertK506@gmail.com>
2013-05-17 15:12:33 +0000

    TrackWriter Statistic

--- RMaps.AppLib/src/com/robert/maps/applib/trackwriter/TrackWriterService.java
+++ RMaps.AppLib/src/com/robert/maps/applib/trackwriter/TrackWriterService.java
@@ -1,6 +1,9 @@
 package com.robert.maps.applib.trackwriter;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 import org.andnav.osm.util.constants.OpenStreetMapConstants;
 
@@ -26,14 +29,16 @@ import android.preference.PreferenceManager;
 import android.widget.Toast;
 
 import com.robert.maps.applib.R;
+import com.robert.maps.applib.kml.Track;
 import com.robert.maps.applib.kml.TrackListActivity;
-import com.robert.maps.applib.trackwriter.IRemoteService;
-import com.robert.maps.applib.trackwriter.ITrackWriterCallback;
 import com.robert.maps.applib.utils.Ut;
 
 public class TrackWriterService extends Service implements OpenStreetMapConstants {
 	private SQLiteDatabase db;
     NotificationManager mNM;
+    Notification mNotification;
+    PendingIntent mContentIntent;
+    final SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
 
 	protected LocationManager mLocationManager;
 	protected SampleLocationListener mLocationListener;
@@ -79,6 +84,10 @@ public class TrackWriterService extends Service implements OpenStreetMapConstant
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		
+		sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+		sdf.applyPattern("HH:mm:ss");
+		
 
 		final File folder = Ut.getRMapsMainDir(this, "data");
 		if(folder.canRead()){
@@ -118,19 +127,19 @@ public class TrackWriterService extends Service implements OpenStreetMapConstant
 		CharSequence text = getText(R.string.remote_service_started);
 
 		// Set the icon, scrolling text and timestamp
-		Notification notification = new Notification(R.drawable.track_writer_service, text, System.currentTimeMillis());
-		notification.flags = notification.flags | Notification.FLAG_NO_CLEAR;
+		mNotification = new Notification(R.drawable.track_writer_service, text, System.currentTimeMillis());
+		mNotification.flags = mNotification.flags | Notification.FLAG_NO_CLEAR;
 
 		// The PendingIntent to launch our activity if the user selects this notification
-		PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+		mContentIntent = PendingIntent.getActivity(this, 0,
 				new Intent(this, TrackListActivity.class), 0);
 
 		// Set the info for the views that show in the notification panel.
-		notification.setLatestEventInfo(this, getText(R.string.remote_service_started), text, contentIntent);
+		mNotification.setLatestEventInfo(this, getText(R.string.remote_service_started), text, mContentIntent);
 
 		// Send the notification.
 		// We use a string id because it is a unique number. We use it later to cancel.
-		mNM.notify(R.string.remote_service_started, notification);
+		mNM.notify(R.string.remote_service_started, mNotification);
 	}
 
 	@Override
@@ -177,6 +186,21 @@ public class TrackWriterService extends Service implements OpenStreetMapConstant
 		private double mDistanceFromLastWriting = 0;
 		private long mTimeFromLastWriting = 0;
 
+		//public Track.TrackPoint lastpt = null;
+		float[] results = {0};
+		public Date lastDate = new Date();
+		public double Distance = 0;
+		public double Duration = 0;
+		public Date Date1 = new Date();
+		public Date Date2 = new Date();
+		public double MaxSpeed = 0;
+		public double AvgSpeed = 0;
+		public double AvgPace = 0;
+		public double MinEle = 99999;
+		public double MaxEle = -99999;
+		public int MoveTime = 0;
+		public double AvgMoveSpeed = 0;
+		
 		public void onLocationChanged(final Location loc) {
 			if (loc != null){
 				boolean needWrite = false;
@@ -193,13 +217,54 @@ public class TrackWriterService extends Service implements OpenStreetMapConstant
 					needWrite = true;
 
 				if(needWrite){
-					//Ut.dd("addPoint mDistanceFromLastWriting="+mDistanceFromLastWriting+" mTimeFromLastWriting="+(mTimeFromLastWriting/1000));
+					final long time = System.currentTimeMillis();
+
+					if(mLastWritedLocation != null) {
+						results[0] = 0;
+						Location.distanceBetween(mLastWritedLocation.getLatitude(), mLastWritedLocation.getLongitude(), loc.getLatitude(), loc.getLongitude(), results);
+						Distance += results[0];
+					} else {
+						Date1.setTime(time);
+						lastDate.setTime(time);
+					}
+
 					mLastWritedLocation = loc;
 					mLastLocation = loc;
 					mDistanceFromLastWriting = 0;
-					addPoint(loc.getLatitude(), loc.getLongitude(), loc.getAltitude(), loc.getSpeed(), System.currentTimeMillis());
+					addPoint(loc.getLatitude(), loc.getLongitude(), loc.getAltitude(), loc.getSpeed(), time);
 
 					mHandler.sendMessage(mHandler.obtainMessage(1, loc));
+					
+					// Statistic
+					Duration = (time - Date1.getTime()) / 1000;
+					if(Duration > 0)
+						AvgSpeed = (Distance / 1000) / (Duration/60/60);
+					if(Distance > 0)
+						AvgPace = Duration / (Distance / 1000);
+					
+					if(loc.getSpeed() > MaxSpeed)
+						MaxSpeed = loc.getSpeed();
+					if(loc.getAltitude() > MaxEle)
+						MaxEle = loc.getAltitude();
+					if(loc.getAltitude() < MinEle)
+						MinEle = loc.getAltitude();
+					if(loc.getSpeed() > 0.5)
+						MoveTime += time - lastDate.getTime();
+					Date2.setTime(time);
+					if(MoveTime > 0)
+						AvgMoveSpeed = (Distance / 1000) / (MoveTime/1000/60.0/60.0);
+					
+					lastDate.setTime(time);
+					
+					final String text = ""
+							+sdf.format(new Date((long) (Duration*1000)))
+							+" | "+Ut.formatDistance(TrackWriterService.this, (float) Distance, 0)
+							+" | "+String.format("%.1f", AvgSpeed)+" km/h"
+							;
+					mNotification.setLatestEventInfo(TrackWriterService.this, getText(R.string.remote_service_started), text, mContentIntent);
+					mNM.notify(R.string.remote_service_started, mNotification);
+
+					
 				} else {
 					//Ut.dd("NOT addPoint mDistanceFromLastWriting="+mDistanceFromLastWriting+" mTimeFromLastWriting="+(mTimeFromLastWriting/1000));
 					mLastLocation = loc;
@@ -210,7 +275,7 @@ public class TrackWriterService extends Service implements OpenStreetMapConstant
 		public void Init(int mintime, int mindistance) {
 			mMinTime = mintime;
 			mMinDistance = mindistance;
-			Ut.dd("mintime="+mintime+" mindistance="+mindistance);
+			Ut.d("mintime="+mintime+" mindistance="+mindistance);
 		}
 
 		public void onStatusChanged(String a, int status, Bundle b) {
