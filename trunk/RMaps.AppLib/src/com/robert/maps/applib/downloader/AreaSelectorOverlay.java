736ecc16a6f5c40deb86dc8778f575ecbc27b76f
robertk506@gmail.com <robertk506@gmail.com@dc686b20-874f-11de-a332-fd40ed1fd0be>
2013-05-31 14:25:40 +0000

    v.0.9.2

--- trunk/RMaps.AppLib/src/com/robert/maps/applib/downloader/AreaSelectorOverlay.java
+++ trunk/RMaps.AppLib/src/com/robert/maps/applib/downloader/AreaSelectorOverlay.java
@@ -49,7 +49,7 @@ public class AreaSelectorOverlay extends TileViewOverlay {
 	
 	private Bitmap getPic(TileView tileView) {
 		if(mCornerMarker == null)
-			mCornerMarker = BitmapFactory.decodeResource(tileView.getContext().getResources(), R.drawable.person);
+			mCornerMarker = BitmapFactory.decodeResource(tileView.getContext().getResources(), R.drawable.r_mark);
 		
 		return mCornerMarker;
 	}
